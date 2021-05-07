a2891ca77b3ccae3457e1b0b02ed685b36f2711d
Manthan447 <76776298+Manthan447@users.noreply.github.com>
2021-05-07 11:36:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-names = []
-function submit() {
-    var name1 = document.getElementById("student_1").value;
-    var name2 = document.getElementById("student_2").value;
-    var name3 = document.getElementById("student_3").value;
-    var name4 = document.getElementById("student_4").value;
-    names.push(name1)
-    names.push(name2)
-    names.push(name3)
-    names.push(name4)
-    document.getElementById("names").innerHTML = names;
-    document.getElementById("sort").style.display = "inline-block";
-    document.getElementById("submit").style.display ="none"
-}
-function sort() {
-    document.getElementById("names").innerHTML = names.sort();
+var names =["Avadhoot Berde", "Arya Berde","Manthan Berde","Nidhish Berde"];
+var images =["https://image.shutterstock.com/image-vector/happy-young-father-holding-his-600w-731506243.jpg","https://st2.depositphotos.com/1967477/8228/v/950/depositphotos_82289046-stock-illustration-cartoon-happy-family-mother-holding.jpg","https://w7.pngwing.com/pngs/665/604/png-transparent-baby-boss-boy-character-illustration-the-boss-baby-brother-dreamworks-animation-film-the-boss-baby-child-toddler-boy.png","https://miro.medium.com/max/842/1*38QD8tbJ8z7Q4WyI9VC6QA.png"]
+var i=0;
+function change()
+{
+
+    document.getElementById("names").innerHTML= names[i];
+    document.getElementById("img1").src = images[i];
+ i++;
+ console.log(i);
+ var number = 5;
+ if(i== number)
+ {
+     i=0;
+ }
 }
